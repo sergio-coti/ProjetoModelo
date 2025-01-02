92b242424efcc96c90ce34277e0adef117122851
sergio-coti <sergio@sistemapeoplenet.com.br>
2025-01-02 18:52:03 -0300

    Criação de controlador de exemplo

---
+++ ProjetoModelo/Controllers/ExemplosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoModelo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExemplosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Teste mensagem!");
+        }
+    }
+}
